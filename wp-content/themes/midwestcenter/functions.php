08c5302b345002b33aa1ce1bc80898b129100a72
tylermachado <tyler@inn.org>
2018-06-21 14:29:36 -0400

    addition of login redirect

--- wp-content/themes/midwestcenter/functions.php
+++ wp-content/themes/midwestcenter/functions.php
@@ -3,3 +3,18 @@
  * Child theme funcitons for mwcir
  */
  define('INN_MEMBER',true);
+
+add_filter( 'leaky_paywall_login_form_args', 'zeen101_login_referrer_redirect' );
+
+function zeen101_login_referrer_redirect( $args ) {
+
+	$referrer = $_SERVER['HTTP_REFERER'];
+
+	if ( !$referrer ) {
+		return $args;
+	}
+
+	$args['redirect'] = $referrer;
+
+	return $args;
+}
