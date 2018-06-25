8b807c7309a3da77dc8d128592a35db59c7e7265
tylermachado <tyler@inn.org>
2018-06-25 14:30:39 -0400

    adding functions to remove WP admin bar for regular subscribers

--- wp-content/themes/midwestcenter/functions.php
+++ wp-content/themes/midwestcenter/functions.php
@@ -4,6 +4,10 @@
  */
  define('INN_MEMBER',true);
 
+
+
+/* return user to previous page after they've logged in */
+
 add_filter( 'leaky_paywall_login_form_args', 'zeen101_login_referrer_redirect' );
 
 function zeen101_login_referrer_redirect( $args ) {
@@ -17,4 +21,31 @@ function zeen101_login_referrer_redirect( $args ) {
 	$args['redirect'] = $referrer;
 
 	return $args;
+}
+
+
+
+/* use email address as username */
+
+add_filter('leaky_paywall_userdata_before_user_create', 'zeen101_force_email_for_new_user' );
+function zeen101_force_email_for_new_user( $userdata ) {
+	$userdata['user_login'] = $userdata['user_email'];
+	return $userdata;
+
+
+
+
+// don't allow users to access WordPress admin
+add_action('admin_init', 'endo_hide_dashboard');
+function endo_hide_dashboard() {
+ if ( ! current_user_can( 'manage_options' ) && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
+  	wp_redirect(home_url()); exit;
+  }
+}
+// remove admin bar from non admin users
+add_action('after_setup_theme', 'endo_remove_admin_bar');
+function endo_remove_admin_bar() {
+	if (!current_user_can('manage_options') && !is_admin()) {
+		show_admin_bar(false);
+	}
 }
