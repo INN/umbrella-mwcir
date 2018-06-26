b3bb42c5c3e97e8d1efd4e42d9df7b3da797f122
tylermachado <tyler@inn.org>
2018-06-26 10:37:05 -0400

    reorganizing CSS file / adding wall for no articles left

--- wp-content/themes/midwestcenter/js/mwcir.js
+++ wp-content/themes/midwestcenter/js/mwcir.js
@@ -1,4 +1,24 @@
 jQuery(document).ready( function($) {
 
+	$('#issuem-leaky-paywall-articles-remaining-subscribe-link').html('<a href="https://investigatemidwest.mwcir.test/subscription-options/">Subscribe now</a>');
+
+
+
+	
+
+	$('#issuem-leaky-paywall-articles-remaining-nag #issuem-leaky-paywall-articles-remaining-text').text(' free articles left.');
+
+	$('<div id="issuem-leaky-paywall-articles-remaining-text-contd">Please subscribe to continue reading and to support our mission.</div>').insertAfter( '#issuem-leaky-paywall-articles-remaining-nag #issuem-leaky-paywall-articles-remaining-text' );
+
+	$('#issuem-leaky-paywall-articles-remaining-nag #issuem-leaky-paywall-articles-remaining-close').click(function() {
+	  $(this).parent().remove();
+	});
+
+
+
+
+	$('#issuem-leaky-paywall-articles-zero-remaining-nag #issuem-leaky-paywall-articles-remaining').empty();
+
+	$('#issuem-leaky-paywall-articles-zero-remaining-nag #issuem-leaky-paywall-articles-remaining').html('<p>We need your support to continue bringing you this content.</p><p>Please consider subscribing today.</p>');
 	
 });
