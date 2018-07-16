ddc47d2eba262dd7ba38ebfa8e234c6cdfa14e3d
tylermachado <tyler@inn.org>
2018-07-16 09:34:28 -0400

    trying to fix button styles to be consistent

--- wp-content/themes/midwestcenter/functions.php
+++ wp-content/themes/midwestcenter/functions.php
@@ -5,8 +5,6 @@
  define('INN_MEMBER',true);
 
 
-
-
 /**
  * get other scripts
  */
@@ -16,9 +14,7 @@
 add_action( 'wp_enqueue_scripts', 'mwcir_enqueue' );
 
 
-
 /* return user to previous page after they've logged in */
-
 add_filter( 'leaky_paywall_login_form_args', 'zeen101_login_referrer_redirect' );
 
 function zeen101_login_referrer_redirect( $args ) {
@@ -35,9 +31,7 @@ function zeen101_login_referrer_redirect( $args ) {
 }
 
 
-
 /* use email address as username */
-
 add_filter('leaky_paywall_userdata_before_user_create', 'zeen101_force_email_for_new_user' );
 function zeen101_force_email_for_new_user( $userdata ) {
 	$userdata['user_login'] = $userdata['user_email'];
@@ -46,17 +40,43 @@ function zeen101_force_email_for_new_user( $userdata ) {
 
 
 
-// don't allow users to access WordPress admin
+/* don't allow users to access WordPress admin */
 add_action('admin_init', 'endo_hide_dashboard');
 function endo_hide_dashboard() {
  if ( ! current_user_can( 'manage_options' ) && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
   	wp_redirect(home_url()); exit;
   }
 }
-// remove admin bar from non admin users
+
+
+/* remove admin bar from non admin users */
 add_action('after_setup_theme', 'endo_remove_admin_bar');
 function endo_remove_admin_bar() {
 	if (!current_user_can('manage_options') && !is_admin()) {
 		show_admin_bar(false);
 	}
+}
+
+/* Add extra text to subscription card options */
+add_filter( 'leaky_paywall_subscription_options_allowed_content', 'zeen101_show_level_benefits_on_card', 10, 3 );
+function zeen101_show_level_benefits_on_card( $allowed_content, $level_id, $level ) {
+	switch ( $level_id ) {
+		case 0:
+			$output = '<ul>';
+			$output .= '<li>Access to all InvestigateMidwest stories.</li>';
+			$output .= '</ul>';
+			break;
+		case 1:
+			$output = '<ul>';
+			$output .= '<li>Access to all InvestigateMidwest stories.</li>';
+			$output .= '<li>Platinum Benefit #2</li>';
+			$output .= '<li>Platinum Benefit #3</li>';
+			$output .= '</ul>';
+			break;
+		
+		default:
+			$output = $allowed_content;
+			break;
+	}
+	return $output;
 }
